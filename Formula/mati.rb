a42aea1e619f75ff2bb973b127188dfa30f84a9c
ioni <ioni_dev@protonmail.com>
2026-07-09 17:58:10 -0600

    mati 0.1.3

--- Formula/mati.rb
+++ Formula/mati.rb
@@ -1,30 +1,30 @@
 class Mati < Formula
   desc "Enforcement layer that gates what AI agents read and edit in your code"
   homepage "https://github.com/ioni-dev/mati"
-  version "0.1.1"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ioni-dev/mati/releases/download/v#{version}/mati-aarch64-apple-darwin.tar.gz"
-      sha256 "3d679c8f601dc222ac12a9245a0a1ac8aa57bd934c72f2e93d841c7da43a4788"
+      sha256 "1153005cb443bfebabf19ae9683caa0e779cdef23c8c703c5c2f4d7601d80499"
     end
 
     on_intel do
       url "https://github.com/ioni-dev/mati/releases/download/v#{version}/mati-x86_64-apple-darwin.tar.gz"
-      sha256 "dd14404ded6c66937af24a0b9b67a3eeee496a4f660da8353cdc389a79309128"
+      sha256 "3309e4c56c2904fe06ae4157275dff2a18ee06ad27205b1961b543c86859dab2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ioni-dev/mati/releases/download/v#{version}/mati-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "e79efcda852cd2187d2c64ade962da8edfd60e5f2b7f9cb3f7578834117495ca"
+      sha256 "1074182eb2e77905e516fef36bb4a0971dd31eefa8e7eb02eaed30900e9fb5bc"
     end
 
     on_intel do
       url "https://github.com/ioni-dev/mati/releases/download/v#{version}/mati-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "2c85011eb06ab73917a3f799b00ba728bad99cf2303d93ec0f8d5c34024bdec1"
+      sha256 "b0c45c2f410029cef3beb301020737a09a56e9e2ee61183582d5cd257ff31aab"
     end
   end
 
